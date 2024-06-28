c8b3a26e3ba8cf3b6afe9da16eb536ca4a63d30f
Naga-74 <158281808+Naga-74@users.noreply.github.com>
2024-06-28 18:33:21 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
     fmt.Println("H e l lo, Wor ld!")
     fmt.Println("He llo,n ewworld!")
-    fmt.Println("so m es dfc   han ge snext next")
+    fmt.Println("so m es dfc   han ge s next next")
     fmt.Println("He llo,World!")
     fmt.Println("ch  d   an   ge d")
 }
